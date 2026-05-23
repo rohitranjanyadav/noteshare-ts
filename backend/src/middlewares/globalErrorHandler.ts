8f07b74c18226c0255c96f78f7f54cdb17d0fe7f
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-05-23 22:10:52 +0545

    create global error handler

--- backend/src/middlewares/globalErrorHandler.ts
+++ backend/src/middlewares/globalErrorHandler.ts
@@ -1,5 +1,22 @@
 import type { NextFunction, Request, Response } from "express";
+import type { HttpError } from "http-errors";
+import envConfig from "../config/config.ts";
 
-const globalErrorHandler = (err,req:Request,res:Response,next:NextFunction)=>{
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = err.statusCode || 500;
 
-}
+  res.status(statusCode).json({
+    message: err.message,
+    errorStack:
+      envConfig.environment === "development"
+        ? err.stack
+        : "Something Went Wrong!!!",
+  });
+};
+
+export default globalErrorHandler;
